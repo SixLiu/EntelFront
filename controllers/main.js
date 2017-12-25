2b0a0f321f618515cd84b30ee10a8783169a2c08
sliu <sliu@aaxiscommerce.com>
2017-12-25 19:17:26 +0800

    update

--- controllers/main.js
+++ controllers/main.js
@@ -8,7 +8,7 @@ module.exports.index = async(ctx) => {
 	if (ctx.isAuthenticated()) {
 		user = ctx.session.passport.user;
 	}
-	const data = {"slides": [{"template": "bar1", "data": {"video": "111"}}, {"template": "bar2", "data": {"video": "111"}}]};
+	const data = {"slides": [{"template": "bar1", "data": {"video": "data1","template":"bar3"}}, {"template": "bar2", "data": {"video": "data2"}}]};
 
 	await ctx.render("index", data);
 
